d56c7e2b19d2e9850e4171bd53e7461a08a28e4d
MJ <michjord0001@gmail.com>
2020-12-13 19:38:37 +0000

    Quick fix for drawing robot objects (breaking conventions)

--- src/covidTracker/CovidRobot.java
+++ src/covidTracker/CovidRobot.java
@@ -1,5 +1,8 @@
 package covidTracker;
 
+import java.awt.Point;
+
+@SuppressWarnings("serial")
 public class CovidRobot {
 
 	private int size;
@@ -8,17 +11,17 @@ public class CovidRobot {
 	private boolean hasConnection;
 	private boolean hasPower;
 	private boolean hasInfection;
-	private int coordinates;
+	public Point coordinates;
 	private String orientation;
 	
-	public CovidRobot(String name, int size, boolean hasConnection, String role, boolean hasPower, boolean infStat, int coord, String orient) {
+	public CovidRobot(String name, int size, boolean hasConnection, String role, boolean hasPower, boolean infStat, String orient) {
 		this.size = size;
 		this.name = name;
 		this.hasConnection = hasConnection;
 		this.role = role;
 		this.hasPower = hasPower;
 		this.hasInfection = hasInfection;
-		this.coordinates = coord;
+		this.coordinates = new Point ((int)(Math.random()*(500)),(int)(Math.random()*(500))); //Assign random location on canvas to start
 		this.orientation = orient;
         }
 	
@@ -28,6 +31,7 @@ public class CovidRobot {
 		System.out.println(" The robot has power: " + ((hasPower)?"Yes":"No"));
 		System.out.println(" The robot has a connection: " + ((hasConnection)?"Yes":"No"));
 		System.out.println(" Is the robot infected: " + ((hasInfection)?"Yes":"No"));
-		System.out.println(" The robot has location: " + coordinates + " and orientation: " + orientation);
+		System.out.println(" The robot has location X=" + (int)coordinates.getX() + " and Y=" + (int)coordinates.getY() );
+		System.out.println(" The robot has orientation: " + orientation);
 	}
 }
