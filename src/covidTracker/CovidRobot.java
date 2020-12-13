500849a00ccd046fe60813984f1d3b9270e16c4b
MJ <michjord0001@gmail.com>
2020-12-13 17:28:29 +0000

    👌 IMPROVE: More robot features

--- src/covidTracker/CovidRobot.java
+++ src/covidTracker/CovidRobot.java
@@ -2,20 +2,24 @@ package covidTracker;
 
 public class CovidRobot {
 
-	private String name; 
 	private int size;
-	private boolean hasConnection;
+	private String name; 
 	private String role;
+	private boolean hasConnection;
 	private boolean hasPower;
 	private boolean hasInfection;
+	private int coordinates;
+	private String orientation;
 	
-	public CovidRobot(String name, int size, boolean hasConnection, String role, boolean hasPower, boolean infStat) {
-		this.name = name;
+	public CovidRobot(String name, int size, boolean hasConnection, String role, boolean hasPower, boolean infStat, int coord, String orient) {
 		this.size = size;
+		this.name = name;
 		this.hasConnection = hasConnection;
 		this.role = role;
 		this.hasPower = hasPower;
 		this.hasInfection = hasInfection;
+		this.coordinates = coord;
+		this.orientation = orient;
         }
 	
 	public void display() {
@@ -24,5 +28,6 @@ public class CovidRobot {
 		System.out.println(" The robot has power: " + ((hasPower)?"Yes":"No"));
 		System.out.println(" The robot has a connection: " + ((hasConnection)?"Yes":"No"));
 		System.out.println(" Is the robot infected: " + ((hasInfection)?"Yes":"No"));
+		System.out.println(" The robot has location: " + coordinates + " and orientation: " + orientation);
 	}
 }
--- src/covidTracker/CovidRobotInformationApp.java
+++ src/covidTracker/CovidRobotInformationApp.java
@@ -3,10 +3,16 @@ package covidTracker;
 public class CovidRobotInformationApp {
 
 	public static void main(String[] args) {
-
-				CovidRobot robot1 = new CovidRobot("Robot 1", 15 , true, "Medical Robot", true, false);
-				robot1.display();
-			}
+				//String name, int size, boolean hasConnection, String role, boolean hasPower, boolean infStat, int coord[], String orient) {
+				
+		CovidRobot robot1 = new CovidRobot("Robot 1", 15 , true, "Medical Robot", true, false, 40, "North");
+		CovidRobot robot2 = new CovidRobot("Robot 2", 15 , true, "Kitchen Robot", true, false, 50, "West");
+		CovidRobot robot3 = new CovidRobot("Robot 3", 15 , true, "Delivery Robot", true, false, 60, "South");
+		CovidRobot robot4 = new CovidRobot("Robot 4", 15 , true, "Entertainment Robot", true, false, 70, "East");
+		CovidRobot robot5 = new CovidRobot("Robot 5", 15 , true, "Dance Robot", true, false, 80, "North-East");
+		
+		robot1.display();
+	}
 
 }
 
